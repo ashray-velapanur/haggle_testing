64a50832b13b066b1347d4005c6d8a64f63ffdfb
Ashray <ashray@b-eagles.com>
2013-05-03 07:13:54 -0400

    cleaning up

--- mapping/create_csv.py
+++
@@ -1,6 +0,0 @@
-from category import HARMONIZED_CATEGORIES
-
-with open('category_mapping.csv', 'w') as file:
-    for category in HARMONIZED_CATEGORIES:
-        print >> file, category.encode('utf-8') + ',' + HARMONIZED_CATEGORIES[category]['category'].name
-
